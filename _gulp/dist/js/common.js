6fac9a81c7de6bc106905adc881fa9dba75ebc2b
Dmitriy Khristoev <haoss_ut@mail.ru>
2018-10-11 23:48:51 +0300

    popups and fixed

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -70,7 +70,13 @@ $(document).on('ready', function(){
 
   $('.open-popup-link').magnificPopup({
     type: 'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    midClick: true,
+    mainClass: 'popupBg',
+    showCloseBtn: false
+  });
+  $('.popup__close, .popup__wrapper-close').on('click', function(e){
+    e.preventDefault();
+    $.magnificPopup.close();
   });
 
   navMobile();
